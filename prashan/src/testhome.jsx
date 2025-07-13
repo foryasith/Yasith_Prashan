5e5530fbf296cd315a9901b2faea0d059abd018a
foryasith <yasithprashan@gmail.com>
2025-07-13 23:57:22 +0530

    test2

---
+++ prashan/src/testhome.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+function RouteLogger() {
+  const location = useLocation();
+  
+  useEffect(() => {
+    console.log('Route changed to:', location.pathname);
+  }, [location]);
+
+  return null;
+}
+
+// Add inside your Router component:
+<Router>
+  <RouteLogger />
+  {/* ... rest of your app ... */}
+</Router>
