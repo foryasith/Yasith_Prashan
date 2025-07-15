5f4cfce7cb5665f6fa675e8920a475ba388e6a8b
foryasith <yasithprashan@gmail.com>
2025-07-15 23:13:23 +0530

    good

--- prashan/src/App.jsx
+++ prashan/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from 'react-router-dom';
-import Home from './components/homepage/home';
+import Home from './components/homepage/Home';
 import Test from './components/experiencepage/Test';
 import Animation from './Animation';
 
@@ -15,7 +15,7 @@ function App() {
           <Route path="/animation" element={<Animation />} />
           
         </Routes>
-      <h1>home from component</h1>
+      <h1>home from comp      onent</h1>
     </div>
     
     
