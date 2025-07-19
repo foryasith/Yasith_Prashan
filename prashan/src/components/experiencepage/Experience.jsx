cbda9c0b43c859fc19b88a7cf44fef40781eed43
foryasith <yasithprashan@gmail.com>
2025-07-19 21:14:32 +0530

    remove

--- prashan/src/components/experiencepage/Experience.jsx
+++ prashan/src/components/experiencepage/Experience.jsx
@@ -1,38 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from '../Header';
 import './Experience.css';
 
 const Experience = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <div className="experience-page">
       <Header />
       <div className="experience-container">
         <h1 className="experience-title">My Experience</h1>
         
-        {loading ? (
-          <div className="loading-container">
-            <div className="loading-spinner">
-              {[...Array(4)].map((_, i) => (
-                <div key={i} style={{ animationDelay: `${i * 0.15}s` }} />
-              ))}
-            </div>
-            <p className="loading-text">Loading experience...</p>
-          </div>
-        ) : (
-          <div className="update-notice">
-            <div className="update-icon">⏳</div>
-            <p>Experience section will be updated soon...</p>
-          </div>
-        )}
+        <div className="update-notice">
+          <div className="update-icon">⏳</div>
+          <p>Experience section will be updated soon...</p>
+        </div>
       </div>
     </div>
   );
